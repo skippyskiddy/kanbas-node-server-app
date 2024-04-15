a58b9059eb36ac62cdb9371df9607be03252d26c
Elif <elifdaa@gmail.com>
2024-04-15 19:47:44 -0400

    fixes

--- App.js
+++ App.js
@@ -12,8 +12,6 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
-mongoose.set('debug', true);
-
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
 
 mongoose.connect(CONNECTION_STRING)
